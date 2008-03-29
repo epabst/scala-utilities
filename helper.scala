47dfde4e869ed0370fd3bfd30af6796284f55d68
harshad.rj <harshad.rj@5548c5ca-8446-0410-956f-e9d0a6d74bab>
2008-03-29 02:48:51 +0000

    * Added a hex case class * ?? now works recursively on on the chain of super classes
    git-svn-id: http://scala-utilities.googlecode.com/svn/trunk@5 5548c5ca-8446-0410-956f-e9d0a6d74bab

--- helper.scala
+++ helper.scala
@@ -2,69 +2,82 @@ import java.lang.reflect._
 import java.io._
 
 object helper {
-  def ? (obj : AnyRef, verbose : Boolean) = {
-    val objClass = obj.getClass
-    val methods = objClass.getDeclaredMethods
-    val publicMethods = methods.filter(m => Modifier.isPublic(m.getModifiers))
+    def showClass [T] (inputClass : java.lang.Class[T], verbose : Boolean, indent : Int) : Unit = {
+        val methods = inputClass.getDeclaredMethods
+        val publicMethods = methods.filter(m => Modifier.isPublic(m.getModifiers))
+  
+        val className = inputClass.getCanonicalName
 
-    val className = objClass.getCanonicalName
-
-    println (className + "\n------------")
-
-    if (verbose) {
-      publicMethods.foreach(m => println (m.toString.replaceAll(className + "\\.", "")))
-      val publicFields = objClass.getDeclaredFields.filter(f => Modifier.isPublic(f.getModifiers))
-      println ("---Public Fields---")
-      publicFields.foreach(f => println (f))
-    } else {
-      val names = publicMethods.map(m => m.getName).toList.removeDuplicates
-      names.foreach(name => println (name + "( )"))
+        val indentString = (0 until indent).foldLeft("")((x,y) => x +"  ")
+  
+        println (indentString + className + "\n" + indentString + "------------")
+  
+        if (verbose) {
+          publicMethods.foreach(m => println (indentString + m.toString.replaceAll(className + "\\.", "")))
+          val publicFields = inputClass.getDeclaredFields.filter(f => Modifier.isPublic(f.getModifiers))
+          println (indentString + "---Public Fields---")
+          publicFields.foreach(f => println (indentString + f))
+          if (inputClass.getSuperclass != null) {
+              println("")
+              showClass (inputClass.getSuperclass, verbose, indent + 1)
+          }
+        } else {
+          val names = publicMethods.map(m => m.getName).toList.removeDuplicates
+          names.foreach(name => println (indentString + name + "( )"))
+        }
+  
+        
     }
 
-  }
+    def ? (obj : AnyRef, verbose : Boolean) = {
+        val classToShow = obj.getClass
+        showClass(classToShow, verbose, 0)
+    }
+  
+    def ? (obj : AnyRef) : Any = {
+      ? (obj, false)
+    }
 
-  def ? (obj : AnyRef) : Any = {
-    ? (obj, false)
-  }
+    def ?? (obj : AnyRef) = {
+      ? (obj, true)
+    }
 
-  def ?? (obj : AnyRef) = {
-    ? (obj, true)
-  }
+    def exec (cmd : String) = {
+      val runTime = Runtime.getRuntime   
+      val process = runTime.exec (cmd)
+      val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
+      var line : String = null
+  
+      do {
+        line = resultBuffer.readLine
+        if (line != null) {
+          println (line)
+        }
+      } while (line != null)
+  
+      process.waitFor
+      process.exitValue
+    }
 
-  def exec (cmd : String) = {
-    val runTime = Runtime.getRuntime   
-    val process = runTime.exec (cmd)
-    val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
-    var line : String = null
+    def execp (cmd : String) = {
+        val runTime = Runtime.getRuntime   
+        val process = runTime.exec (cmd)
+        val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
+        var line : String = null
+        var lineList : List[String] = Nil
+  
+  
+        do {
+            line = resultBuffer.readLine
+            if (line != null) {
+                lineList = line :: lineList
+            }
+        } while (line != null)
+  
+        process.waitFor
+  
+        (process.exitValue, lineList.reverse)
+    }
 
-    do {
-      line = resultBuffer.readLine
-      if (line != null) {
-        println (line)
-      }
-    } while (line != null)
-
-    process.waitFor
-    process.exitValue
-  }
-
-  def execp (cmd : String) = {
-    val runTime = Runtime.getRuntime   
-    val process = runTime.exec (cmd)
-    val resultBuffer = new BufferedReader(new InputStreamReader(process.getInputStream))
-    var line : String = null
-    var lineList : List[String] = Nil
-
-
-    do {
-      line = resultBuffer.readLine
-      if (line != null) {
-        lineList = line :: lineList
-      }
-    } while (line != null)
-
-    process.waitFor
-
-    (process.exitValue, lineList.reverse)
-  }
+    case class hex(value : Int) { override def toString = "0x" + value.toHexString }
 }
