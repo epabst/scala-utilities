ae4c425dd922ba1143de0b6b0d572f471383c5f4
harshad.rj <harshad.rj@5548c5ca-8446-0410-956f-e9d0a6d74bab>
2008-11-13 08:41:56 +0000

    Helper functions for parsing XML DOMs
    git-svn-id: http://scala-utilities.googlecode.com/svn/trunk@10 5548c5ca-8446-0410-956f-e9d0a6d74bab

---
+++ xml.scala
@@ -0,0 +1,32 @@
+
+object xmlHelper {
+  /** convenience function that provides implicit conversion from Node to an anonymous type
+      with following members:
+      <ul>
+        <li>getChildOpt  gets the first child node with specified label</li>
+        <li>getLabel  finds the first child node with specified label and returns its text value</li>
+        <li>getLong  finds the first child node with specified label and returns its Long value</li>
+        <li>getBool  finds the first child node with specified label and returns its Boolean value</li>
+      </ul> */
+  private implicit def node2Label(node:Node) = new {
+    /** Only gets the first child */
+    def getChildOpt(label:String) = {
+      val children = (node \ label)
+      if (children.length > 0) Some(children(0))
+      else None
+    }
+
+    def getChild(label:String) = (node \ label) (0)
+
+    def getLabelOpt(label:String) = getChildOpt(label) map (_ text)
+    def getLabel(label:String) = getChild(label) text
+
+    def getLongOpt(label:String) = getLabelOpt(label).map(Helper.parseLong(_))
+    def getLong(label:String) = Helper.parseLong(getLabel(label))
+
+    def getBool(label:String) = getLabel(label) == "true"
+    def getBoolOpt(label:String) = getLabelOpt(label) map (_ == "true")
+
+    def getAttrib(name:String) = (node attribute (node namespace, name)).get.apply(0) text
+  }
+}
