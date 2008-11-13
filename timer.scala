41d54be6a17b2b585e986f634a292420dab97100
harshad.rj <harshad.rj@5548c5ca-8446-0410-956f-e9d0a6d74bab>
2008-11-13 09:28:24 +0000

    Timer: useful for testing
    git-svn-id: http://scala-utilities.googlecode.com/svn/trunk@11 5548c5ca-8446-0410-956f-e9d0a6d74bab

---
+++ timer.scala
@@ -0,0 +1,11 @@
+object Timer {
+  var start:Long = 0L
+  var end:Long = 0L
+  def go = {
+    start = System.currentTimeMillis
+  }
+  def stop = {
+    end = System.currentTimeMillis
+    println(">   " + (end - start)/ 1000.0 + " s")
+  }
+}
